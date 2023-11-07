19c49bc5d2f56d898033ed96c186a5bd330d2153
SelamB <selambeyene2@gmail.com>
2023-11-07 15:36:54 -0800

    adds nav bar template

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,16 +4,18 @@ import './App.css';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import AuthPage from '../AuthPage/AuthPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
   const [user, setUser] = useState({})
   return (
     <main className="App">
+      <NavBar />
       { user ? 
-      <Routes>
-        <Route path='/orders/new' element={ <NewOrderPage /> }/>
-        <Route path='/orders' element={ <OrderHistoryPage /> }/>
-      </Routes> 
+        <Routes>
+          <Route path='/orders/new' element={ <NewOrderPage /> }/>
+          <Route path='/orders' element={ <OrderHistoryPage /> }/>
+        </Routes> 
       : 
       <AuthPage /> }
     </main>
