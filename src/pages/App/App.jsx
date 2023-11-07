c5deb1ed03df6039c09d33821379716a4f1689e3
SelamB <selambeyene2@gmail.com>
2023-11-07 15:11:17 -0800

    sets up user state

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,14 @@
-
+import { useState } from 'react';
 import './App.css';
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import AuthPage from '../AuthPage/AuthPage';
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 
 export default function App() {
+  const [user, setUser] = useState(null)
   return (
     <main className="App">
-      APP
+      { user ? <NewOrderPage /> : <AuthPage /> }
     </main>
   );
 }
