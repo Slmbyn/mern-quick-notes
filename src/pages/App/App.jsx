9fb70596b57fd24844aad80a3c823a6001766477
SelamB <selambeyene2@gmail.com>
2023-11-07 15:25:58 -0800

    sets up some client side routing

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import AuthPage from '../AuthPage/AuthPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 
 export default function App() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState({})
   return (
     <main className="App">
-      { user ? <NewOrderPage /> : <AuthPage /> }
+      { user ? 
+      <Routes>
+        <Route path='/orders/new' element={ <NewOrderPage /> }/>
+        <Route path='/orders' element={ <OrderHistoryPage /> }/>
+      </Routes> 
+      : 
+      <AuthPage /> }
     </main>
   );
 }
